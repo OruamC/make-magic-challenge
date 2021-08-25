9ee57f393d8a1877f9c744b2e6cda5efb1c23292
Oruam <oru_catani@hotmail.com>
2021-08-25 20:27:11 -0300

    feat: starting project

---
+++ src/main/java/com/dextra/challenge/makemagic/MakeMagicChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dextra.challenge.makemagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMagicChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMagicChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dextra/challenge/makemagic/MakeMagicChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dextra.challenge.makemagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMagicChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
