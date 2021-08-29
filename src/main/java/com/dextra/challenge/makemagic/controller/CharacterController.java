12be0a59c033f6f932cce369a249f93a6f967fb1
Oruam <oru_catani@hotmail.com>
2021-08-28 22:04:45 -0300

    feat: get by id endpoint

--- src/main/java/com/dextra/challenge/makemagic/controller/CharacterController.java
+++ src/main/java/com/dextra/challenge/makemagic/controller/CharacterController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,11 @@ public class CharacterController {
 	@GetMapping
 	public ResponseEntity<List<CharacterResponseDTO>> getAllCharacters() {
 		return ResponseEntity.ok(this.service.getAllCharacters());
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CharacterResponseDTO> getById(@PathVariable(name = "id") Long id) {
+		return ResponseEntity.ok(this.service.getById(id));
 	}
 	
 	@PostMapping
